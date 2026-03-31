3db07b315859e8ee0fa3b26c91d91f714b8788a4
Da-shou <fazlijaylli@gmail.com>
2026-03-31 21:07:48 +0200

    Improved window creation and destruction
    I forgot to destroy the window at the end of the program, so this was
    added. Also, I checked if GLFW initalize correctly before the rest of
    the program runs.

--- src/main.c
+++ src/main.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdint.h>
 
 #define GLAD_GL_IMPLEMENTATION
 #include <glad/gl.h>
 #include <GLFW/glfw3.h>
 
-const GLuint WIDTH = 800, HEIGHT = 600;
+const uint16_t WIDTH = 800, HEIGHT = 600;
+const char* const WINDOW_TITLE = "da-cubec";
 
 void key_callback(GLFWwindow* window, int key, int scancode, int action,
                   int mode) {
@@ -14,21 +16,32 @@ void key_callback(GLFWwindow* window, int key, int scancode, int action,
 }
 
 int main(void) {
-        glfwInit();
+        if (!glfwInit()) {
+		fprintf(stderr, "%s\n", "GLFW could not initialize. Exiting...");
+		exit(EXIT_FAILURE);
+	}
 
         glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
         glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
         glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-        GLFWwindow* window = glfwCreateWindow(
-            WIDTH, HEIGHT, "[glad] GL with GLFW", NULL, NULL);
+        // Creating window
+        GLFWwindow* window =
+            glfwCreateWindow(WIDTH, HEIGHT, WINDOW_TITLE, NULL, NULL);
         glfwMakeContextCurrent(window);
 
         glfwSetKeyCallback(window, key_callback);
 
+        // Printing compilation and runtime infos
         int version = gladLoadGL(glfwGetProcAddress);
-        printf("GL %d.%d\n", GLAD_VERSION_MAJOR(version),
+        printf("Running on OpenGL %d.%d\n", GLAD_VERSION_MAJOR(version),
                GLAD_VERSION_MINOR(version));
+        printf("Compiled against GLFW %i.%i.%i\n", GLFW_VERSION_MAJOR,
+               GLFW_VERSION_MINOR, GLFW_VERSION_REVISION);
+        int major, minor, revision;
+        glfwGetVersion(&major, &minor, &revision);
+        printf("Running against GLFW %i.%i.%i\n", major, minor, revision);
+        printf("Platform ID %d\n", glfwGetPlatform());
 
         // Main window loop
         while (!glfwWindowShouldClose(window)) {
@@ -38,6 +51,7 @@ int main(void) {
                 glfwSwapBuffers(window);
         }
 
+	glfwDestroyWindow(window);
         glfwTerminate();
 
         return EXIT_SUCCESS;
